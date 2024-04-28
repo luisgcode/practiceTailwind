277d926493e2f989ab5721b5844932882781a20e
luisgcode <luisgcode@gmail.com>
2024-04-28 17:47:37 -0400

    adding dark mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const htmlElement = document.querySelector("html");
+const toggleButton = document.querySelector("#toggle");
+const toggleButtonMobile = document.querySelector("#toggle-mb");
+
+toggleButton.addEventListener("click", function () {
+  htmlElement.classList.toggle("dark");
+});
+
+toggleButtonMobile.addEventListener("click", function () {
+  htmlElement.classList.toggle("dark");
+});
